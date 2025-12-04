c3db9b66c913903cd7df6e9afa992c3de96fff5c
dacanizares <dacanizares@outlook.com>
2025-12-04 11:12:15 -0500

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -2,4 +2,5 @@
 
 mkdir -p bin/dbg-linux
 rm bin/dbg-linux/run > /dev/null 2>&1
-gcc src/main.c -g -ggdb -lraylib -lm -o bin/dbg-linux/run -std=c99 -pedantic
+gcc -Wall -Wextra -Wunused-function -Wunused-variable -Wunused-parameter src/main.c -g -ggdb -lraylib -lm -o bin/dbg-linux/run -std=c99 -pedantic -I/usr/local/include
+
