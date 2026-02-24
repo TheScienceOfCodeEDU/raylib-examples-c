bab2d37dc5f86c766ed008517023aa269a0881b0
dacanizares <dacanizares@outlook.com>
2026-02-23 19:11:59 -0500

    Fixed trail size

--- src/gui/main.h
+++ src/gui/main.h
@@ -626,6 +626,8 @@ void GUI_DrawPointerTrail()
 {
     GUI_PointerSetup *pointer_setup = GUI_GetPointerSetup();
     Vector2 mouse                   = GUI_CTX.temp->mouse_current;
+    Vector2 delta_normalized        = pointer_setup->trail_delta_normalized;
+    float scale                     = pointer_setup->pointer_scale * GUI_CTX.state->scale;
 
     // Shift all existing positions backward by one slot in the array
     // The last element (the oldest position) is dropped
@@ -633,9 +635,10 @@ void GUI_DrawPointerTrail()
     for (int i = GUI_MAX_TRAIL - 1; i > 0; i--) {
         trail[i] = trail[i - 1];
     }
-    Vector2 delta = (Vector2) {
-        pointer_setup->trail_delta_normalized.x * pointer_setup->pointer_texture.width * pointer_setup->pointer_scale,
-        pointer_setup->trail_delta_normalized.y * pointer_setup->pointer_texture.height * pointer_setup->pointer_scale
+
+    Vector2 delta   = (Vector2) {
+        delta_normalized.x * pointer_setup->pointer_texture.width * scale,
+        delta_normalized.y * pointer_setup->pointer_texture.height * scale
     };
     trail[0] = Vector2Add(mouse, delta);
 
@@ -650,7 +653,7 @@ void GUI_DrawPointerTrail()
             // Fade (color, alpha) - alpha is 0.5 to 1.0 based on ratio
             float alpha_min = 0.001f;
             float alpha_max = 0.05f;
-            float trail_size = 5.0f;
+            float trail_size = scale;
             Color trail_color = Fade(WHITE, ratio * (alpha_max - alpha_min) + alpha_min);
 
             // Size effect: oldest positions are smaller
