f2f4a049c9fecf935254ddcbcd7e1a1ea84e89cc
dacanizares <dacanizares@outlook.com>
2026-03-01 11:54:52 -0500

    We may have unused functions exposed

--- run/build-linux.sh
+++ run/build-linux.sh
@@ -2,5 +2,4 @@
 
 mkdir -p bin/dbg-linux
 rm bin/dbg-linux/run > /dev/null 2>&1
-gcc -Wall -Wextra -Wunused-function -Wunused-variable -Wunused-parameter src/main.c -g -ggdb -lraylib -lm -o bin/dbg-linux/run -std=c99 -pedantic -I/usr/local/include
-
+gcc -Wall -Wextra -Wunused-variable -Wunused-parameter src/main.c -g -ggdb -lraylib -lm -o bin/dbg-linux/run -std=c99 -pedantic -I/usr/local/include
